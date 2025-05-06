c6db403b6daa8e4d6cd93576395d04f69e3a06bc
Sunless <sunless17@tutamail.com>
2025-05-06 22:35:20 +0300

    feat: having some ideas of surreal

--- db/src/main.rs
+++ db/src/main.rs
@@ -1,5 +1,4 @@
-use axum::Router;
-use axum::routing::get;
+use axum::{Router, routing::get};
 use std::sync::LazyLock;
 use surrealdb::{
   Surreal,
@@ -11,38 +10,25 @@ static DB: LazyLock<Surreal<Client>> = LazyLock::new(Surreal::init);
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
   // surreal db
+  // connect to local db with <Ws> (remote <Wss>)
   DB.connect::<Ws>("0.0.0.0:8000").await?;
+  // create the namespace and database
+  DB.use_ns("test_namespace").use_db("test_database").await?;
+  // set some queries
+  let sql = "
+  CREATE website:article;
+  SET topic='hello world', time=13, best=false;
+  ";
+  let setup: Vec<_> = DB.select("website").await?;
+  // testing
+  println!("db operational {:?}", setup);
+
+  // axum server
+  let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
+  let router = Router::new().route("/", get(|| async { "Hello, World!" }));
   // use tracing
-  println!("db operational");
-
-  // // axum server
-  // let listener = tokio::net::TcpListener::bind("localhost:8080").await?;
-  // let router = Router::new().route("/", get(paths));
-  // // use tracing
-  // println!("server operational");
-
-  // axum::serve(listener, router).await?;
+  println!("server operational");
+  axum::serve(listener, router).await?;
 
   Ok(())
 }
-
-// async fn paths() -> &'static str {
-//   r#"
-// -----------------------------------------------------------------------------------------------------------------------------------------
-//         PATH                |           SAMPLE COMMAND
-// -----------------------------------------------------------------------------------------------------------------------------------------
-// /session: See session data  |  curl -X GET    -H "Content-Type: application/json"                      http://localhost:8080/session
-//                             |
-// /person/{id}:               |
-//   Create a person           |  curl -X POST   -H "Content-Type: application/json" -d '{"name":"John"}' http://localhost:8080/person/one
-//   Update a person           |  curl -X PUT    -H "Content-Type: application/json" -d '{"name":"Jane"}' http://localhost:8080/person/one
-//   Get a person              |  curl -X GET    -H "Content-Type: application/json"                      http://localhost:8080/person/one
-//   Delete a person           |  curl -X DELETE -H "Content-Type: application/json"                      http://localhost:8080/person/one
-//                             |
-// /people: List all people    |  curl -X GET    -H "Content-Type: application/json"                      http://localhost:8080/people
-
-// /new_user:  Create a new record user
-// /new_token: Get instructions for a new token if yours has expired
-// "#
-// }
- 
