3f6ac5c9f687bf348f6a59171b974255bdfd06d7
Sunless <sunless17@tutamail.com>
2025-05-08 22:05:04 +0300

    bug: require permissions to query the database

--- db/src/main.rs
+++ db/src/main.rs
@@ -10,18 +10,23 @@ static DB: LazyLock<Surreal<Client>> = LazyLock::new(Surreal::init);
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
   // surreal db
+  // TODO: create container with logins for surreal
+  // TODO: use dot envy for logins and ports
   // connect to local db with <Ws> (remote <Wss>)
   DB.connect::<Ws>("0.0.0.0:8000").await?;
   // create the namespace and database
   DB.use_ns("test_namespace").use_db("test_database").await?;
+  // TODO: write queries
   // set some queries
   let sql = "
   CREATE website:article;
-  SET topic='hello world', time=13, best=false;
+  SET topic=13, time=13, best=13;
   ";
-  let setup: Vec<_> = DB.select("website").await?;
+  let setup: Vec<u8> = DB.select("website").await?;
+
   // testing
-  println!("db operational {:?}", setup);
+  println!("db operational");
+  println!("{:?}", setup);
 
   // axum server
   let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
