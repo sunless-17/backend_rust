92c4b742bd5e20158d9ef502a61d5bdd0831759c
Sunless <sunless17@tutamail.com>
2025-05-09 07:53:08 +0300

    feat: successfully queried an entry to surreal and got a response

--- db/src/main.rs
+++ db/src/main.rs
@@ -1,44 +1,92 @@
 use axum::{Router, routing::get};
+use serde::Serialize;
 use std::sync::LazyLock;
 use surrealdb::{
-  Surreal,
+  Response, Surreal,
   engine::remote::ws::{Client, Ws},
   opt::auth::Root,
 };
 
 static DB: LazyLock<Surreal<Client>> = LazyLock::new(Surreal::init);
 
+#[derive(Debug, Serialize)]
+struct Person {
+  fname: String,
+  lname: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
   // surreal db
-  // TODO: create container with logins for surreal
   // TODO: use dot envy for logins and ports
   // connect to local db with <Ws> (remote <Wss>)
   DB.connect::<Ws>("0.0.0.0:8000").await?;
 
-  // authenticate
-  DB.signin(Root {
-    username: "root",
-    password: "root",
-  })
-  .await?;
+  // authenticate with surrealdb
+  let auth = DB
+    .signin(Root {
+      username: "root",
+      password: "root",
+    })
+    .await?;
+
+  // // proper authentication
+  //   let token = DB
+  //     .signin(Scope {
+  //       namespace: "test_namespace",
+  //       database: "test_database",
+  //       access: "user",
+  //       params: Credentials {
+  //         username: "root",
+  //         pass: "root",
+  //       },
+  //     })
+  //     .await?;
+  //   DB.authenticate(token).await?;
 
   // create the namespace and database
   DB.use_ns("test_namespace").use_db("test_database").await?;
 
-  // TODO: write queries on a .suql
-  // set some queries
-  let sql = "
-  CREATE website:article;
-  SET topic=13, time=13, best=13;
-  ";
+  // setting variables to be used in the db
+  DB.set(
+    "name",
+    Person {
+      fname: "noice".to_string(),
+      lname: "noie".to_string(),
+    },
+  )
+  .await?;
 
-  // getting the data
-  let setup: Vec<u8> = DB.select("website").await?;
+  // query the variables to the database
+  DB.query("CREATE people SET first_person = $name").await?;
+
+  // reponse from the database
+  let ress: Response = DB.query("SELECT * FROM people").await?;
+  dbg!(ress);
+
+  // // Create a new person with a random id
+  // let created: Option<Person> = DB
+  //   .create("people")
+  //   .content(Person {
+  //     name: "noice".to_string(),
+  //     age: 13,
+  //   })
+  //   .await?;
+  // dbg!(created);
+
+  // // // Update a person record with a specific id
+  // // let updated: Option<Record> = db
+  // //   .update(("person", "jaime"))
+  // //   .merge(Responsibility { marketing: true })
+  // //   .await?;
+  // // dbg!(updated);
+
+  // // Select all people records
+  // let people: Vec<Person> = DB.select("people").await?;
+  // dbg!(people);
 
   // testing
   println!("db operational");
-  println!("{:?}", setup);
 
   // axum server
   let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
---
+++ notes.md
@@ -0,0 +1,4 @@
+# quest
+- better auth
+- send queries < create suql
+- crud
