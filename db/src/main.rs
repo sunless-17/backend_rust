ed0bbc7763b1ef8597256004622115c86720ca77
Sunless <sunless17@tutamail.com>
2025-05-09 06:50:03 +0300

    feat: authenticate database

--- db/src/main.rs
+++ db/src/main.rs
@@ -3,6 +3,7 @@ use std::sync::LazyLock;
 use surrealdb::{
   Surreal,
   engine::remote::ws::{Client, Ws},
+  opt::auth::Root,
 };
 
 static DB: LazyLock<Surreal<Client>> = LazyLock::new(Surreal::init);
@@ -14,14 +15,25 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   // TODO: use dot envy for logins and ports
   // connect to local db with <Ws> (remote <Wss>)
   DB.connect::<Ws>("0.0.0.0:8000").await?;
+
+  // authenticate
+  DB.signin(Root {
+    username: "root",
+    password: "root",
+  })
+  .await?;
+
   // create the namespace and database
   DB.use_ns("test_namespace").use_db("test_database").await?;
-  // TODO: write queries
+
+  // TODO: write queries on a .suql
   // set some queries
   let sql = "
   CREATE website:article;
   SET topic=13, time=13, best=13;
   ";
+
+  // getting the data
   let setup: Vec<u8> = DB.select("website").await?;
 
   // testing
