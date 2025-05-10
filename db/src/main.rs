2694a533bcf9c688d3b3d3e8dc830487b6fbd030
Sunless <sunless17@tutamail.com>
2025-05-10 17:52:02 +0300

    docs: setup container for surrealkv

--- db/src/main.rs
+++ db/src/main.rs
@@ -15,7 +15,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   // connect to local db with <Ws> (remote <Wss>)
   // db.connect::<Ws>("DB_PORT").await?;
   // using surreakv for embedded database
-  let db = connect("surrealkv://mydata").await?;
+  let db = connect("surrealkv://db").await?;
 
   // authenticate with surrealdb
   db.signin(Root {
@@ -27,23 +27,23 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
   // create the namespace and database
   db.use_ns("test_namespace").use_db("test_database").await?;
 
-  // // posting records from the database
-  // // FIX: add type annotations without assigning a variable
-  // let record: Option<Person> = db
-  //   .create(("table_name", 1))
-  //   .content(Person {
-  //     fname: "foo".to_string(),
-  //     lname: "bar".to_string(),
-  //   })
-  //   .await?;
+  // posting records from the database
+  // FIX: add type annotations without assigning a variable
+  let record: Option<Person> = db
+    .create(("table_name", 0))
+    .content(Person {
+      fname: "foo".to_string(),
+      lname: "bar".to_string(),
+    })
+    .await?;
 
   // reading records from the database
   // vec for entire record response
   let res: Vec<Person> = db.select("table_name").await?;
   dbg!(&res);
   // opt for specific record
-  let ress: Option<Person> = db.select(("table_name", 1)).await?;
-  dbg!(ress);
+  let ress: Option<Person> = db.select(("table_name", 0)).await?;
+  dbg!(&ress);
 
   // // updating records from the database
   // let record: Option<Person> = DB
@@ -75,7 +75,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
   // axum server
   let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
-  let router = Router::new().route("/", get(|| async { "hello world!" }));
+  let router = Router::new().route("/", get(move || async { "hello world!" }));
   // use tracing
   println!("server operational");
   axum::serve(listener, router).await?;
--- notes.md
+++ notes.md
@@ -2,3 +2,17 @@
 - better auth
 - send queries < create suql
 - crud
+# docker
+- setup surrealkv (remember to add it as a feature to surrealdb dependency)
+```bash
+docker run \
+    --name db \
+    --volume $(pwd)/<dir_name>:/<dir_name> \
+    --publish 8000:8000 \
+    # give docker permissions to edit and create stuffs in the folder 
+    --user $(id -u) \
+    surrealdb/surrealdb:latest start \
+    --user <user_name> \
+    --pass <user_password> \
+    surrealkv://<dir_name>
+```
