9e6259850324ec9887622d08a632e9396721f8ab
Sunless <sunless17@tutamail.com>
2025-05-10 17:17:40 +0300

    feat: use embedded database with surrealkv

--- db/src/main.rs
+++ db/src/main.rs
@@ -1,13 +1,6 @@
 use axum::{Router, routing::get};
 use serde::{Deserialize, Serialize};
-use std::sync::LazyLock;
-use surrealdb::{
-  Surreal,
-  engine::remote::ws::{Client, Ws},
-  opt::auth::Root,
-};
-
-static DB: LazyLock<Surreal<Client>> = LazyLock::new(Surreal::init);
+use surrealdb::{engine::any::connect, opt::auth::Root};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Person {
@@ -18,70 +11,71 @@ struct Person {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
   // surreal db
-  // TODO: use dot envy for logins and ports
   // TODO: using tracing to log
   // connect to local db with <Ws> (remote <Wss>)
-  DB.connect::<Ws>("0.0.0.0:8000").await?;
+  // db.connect::<Ws>("DB_PORT").await?;
+  // using surreakv for embedded database
+  let db = connect("surrealkv://mydata").await?;
 
   // authenticate with surrealdb
-  DB.signin(Root {
+  db.signin(Root {
     username: "root",
     password: "root",
   })
   .await?;
 
   // create the namespace and database
-  DB.use_ns("test_namespace").use_db("test_database").await?;
+  db.use_ns("test_namespace").use_db("test_database").await?;
 
-  // posting records from the database
-  // FIX: add type annotations without assigning a variable
-  let record: Option<Person> = DB
-    .create(("table_name", 0))
-    .content(Person {
-      fname: "foo".to_string(),
-      lname: "bar".to_string(),
-    })
-    .await?;
-
-  // reading records from the database
-  // vec for entire record response
-  let res: Vec<Person> = DB.select("table_name").await?;
-  dbg!(res);
-  // opt for specific record
-  let ress: Option<Person> = DB.select(("table_name", 0)).await?;
-  dbg!(ress);
-
-  // updating records from the database
-  let record: Option<Person> = DB
-    .update(("table_name", 0))
-    .content(Person {
-      fname: "foo bar".to_string(),
-      lname: "bar foo".to_string(),
-    })
-    .await?;
+  // // posting records from the database
+  // // FIX: add type annotations without assigning a variable
+  // let record: Option<Person> = db
+  //   .create(("table_name", 1))
+  //   .content(Person {
+  //     fname: "foo".to_string(),
+  //     lname: "bar".to_string(),
+  //   })
+  //   .await?;
 
   // reading records from the database
   // vec for entire record response
-  let res: Vec<Person> = DB.select("table_name").await?;
-  dbg!(res);
+  let res: Vec<Person> = db.select("table_name").await?;
+  dbg!(&res);
   // opt for specific record
-  let ress: Option<Person> = DB.select(("table_name", 0)).await?;
+  let ress: Option<Person> = db.select(("table_name", 1)).await?;
   dbg!(ress);
 
-  // delete records and entire tables on the database
+  // // updating records from the database
+  // let record: Option<Person> = DB
+  //   .update(("table_name", 0))
+  //   .content(Person {
+  //     fname: "foo bar".to_string(),
+  //     lname: "bar foo".to_string(),
+  //   })
+  //   .await?;
+
+  // // reading records from the database
   // // vec for entire record response
-  let del: Vec<Person> = DB.delete("table_name").await?;
-  dbg!(del);
-  // opt for specific record
-  let dell: Option<Person> = DB.delete(("table_name", "0")).await?;
-  dbg!(dell);
+  // let res: Vec<Person> = DB.select("table_name").await?;
+  // dbg!(res);
+  // // opt for specific record
+  // let ress: Option<Person> = DB.select(("table_name", 0)).await?;
+  // dbg!(ress);
+
+  // // delete records and entire tables on the database
+  // // // vec for entire record response
+  // let del: Vec<Person> = DB.delete("table_name").await?;
+  // dbg!(del);
+  // // opt for specific record
+  // let dell: Option<Person> = DB.delete(("table_name", "0")).await?;
+  // dbg!(dell);
 
   // testing
   println!("db connected");
 
   // axum server
   let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await?;
-  let router = Router::new().route("/", get(|| async { "Hello, World!" }));
+  let router = Router::new().route("/", get(|| async { "hello world!" }));
   // use tracing
   println!("server operational");
   axum::serve(listener, router).await?;
